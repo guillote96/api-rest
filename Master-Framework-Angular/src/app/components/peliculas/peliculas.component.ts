84fac28a168a2f1afc383dbddd541ab9d2ab4a7e
Guille <juarez.unlp96@gmail.com>
2021-05-03 17:37:02 -0300

    comunicacion entre componentes y impl. modelo

--- Master-Framework-Angular/src/app/components/peliculas/peliculas.component.ts
+++ Master-Framework-Angular/src/app/components/peliculas/peliculas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit,DoCheck,OnDestroy} from '@angular/core';
+import { Pelicula } from 'src/app/models/pelicula';
 
 @Component({
   selector: 'app-peliculas',
@@ -6,11 +7,11 @@ import { Component, OnInit,DoCheck,OnDestroy} from '@angular/core';
   styleUrls: ['./peliculas.component.css']
 })
 export class PeliculasComponent implements OnInit,DoCheck,OnDestroy {
-  public peliculas:Array<any>;
+  public peliculas:Pelicula[];
   constructor() { 
     this.peliculas =[
-      {title:"Harry Potter y la Piedra Filosofal", image:"https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2016/12/harry-potter-piedra-filosofal.jpg?itok=1_pqQxcN"},
-      {title:"Harry Potter y la camara de los secretos", image:"https://comoacaba.com/wp-content/uploads/2018/11/bhCfAzeMMg7GyLDT11yVM2i1NPh.jpg"}
+      new Pelicula("Harry Potter y la Piedra Filosofal","https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2016/12/harry-potter-piedra-filosofal.jpg?itok=1_pqQxcN"),
+      new Pelicula("Harry Potter y la camara de los secretos","https://comoacaba.com/wp-content/uploads/2018/11/bhCfAzeMMg7GyLDT11yVM2i1NPh.jpg")
     ];
   }
 
---
+++ Master-Framework-Angular/src/app/models/pelicula.ts
@@ -0,0 +1,7 @@
+export class Pelicula{
+    constructor(
+        public title:string,
+        public image:string
+    ){}
+
+}
